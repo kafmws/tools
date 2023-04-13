cedc5788c91355f9392b3ff5639cf80dc7ff932c
kafmws <740721665@qq.com>
2023-04-13 13:57:19 +0800

    clean

--- simple Linux command/Untitled1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	char str[100];
-	scanf("%[^\n]%*c", str);
-	printf("%s",str);
-	perror("test error");
-	return 0;
-}
--- simple Linux command/socket.cpp
+++
@@ -1,9 +0,0 @@
-#include<unistd.h>
-#include<sys/socket.h> 
-
-int main(){
-	
-	int listensocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	listen(listensocket, 10);
-	return 0;
-}
