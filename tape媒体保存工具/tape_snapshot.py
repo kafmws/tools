e0c347348cf6b14d0ec1d7497fcda7d8513e3e59
kafmws <740721665@qq.com>
2023-04-13 20:38:54 +0800

    纪念死去的tape

--- tape媒体保存工具/tape_snapshot.py
+++ tape媒体保存工具/tape_snapshot.py
@@ -1,25 +1,27 @@
 import os
 import re
+import sys
 import time
 import shutil
 import requests
 
 from tqdm import tqdm
 
-source_path = 'D:/Torrent/15955897608' # 设置为导出的手机号码目录路径
+source_path = 'tape.zip' # 设置为导出的手机号码目录路径 或 zip文件路径
 target_path = None
 
-def save_batch(text, work_dir, urls, static_path, sleep_time=0.2):
-    if len(urls) <= 0:
+def save_batch(text, work_dir, url_pairs, static_path, sleep_time=0.2):
+    if len(url_pairs[0]) <= 0:
         return text
     os.chdir(work_dir)
-    for url in tqdm(urls, ncols=60):
+    urls, orignal_urls = url_pairs
+    for to_repalce, url in tqdm(list(zip(urls, orignal_urls)), ncols=60):
         filename = url.split('/')[-1]
         response = requests.get(url)
         with open(filename, 'wb') as f:
             f.write(response.content)
         time.sleep(sleep_time)
-        text = text.replace(url, os.path.join(static_path, filename))
+        text = text.replace(to_repalce, os.path.join(static_path, filename))
     return text
 
 def save(userdir):
@@ -48,21 +50,40 @@ def save(userdir):
             text = source.read()
             
             img_urls = re.findall(r'<img.*?src="(.*?)".*?>', text, re.S)
-            img_urls = [url.split('?')[0] for url in img_urls]
-            text = save_batch(text, pic_dir, img_urls, os.path.join(static_path, 'picture'))
+            orignal_img_urls = [url.split('?')[0] for url in img_urls]
+            text = save_batch(text, pic_dir, (img_urls, orignal_img_urls), os.path.join(static_path, 'picture'))
             
             audio_urls = re.findall(r'<audio.*?<source src="(.*?)".*?>', text, re.S)
-            text = save_batch(text, audio_dir, audio_urls, os.path.join(static_path, 'audio'))
+            text = save_batch(text, audio_dir, (audio_urls, audio_urls), os.path.join(static_path, 'audio'))
             
             video_urls = re.findall(r'<video.*?<source src="(.*?)".*?>', text, re.S)
-            text = save_batch(text, video_dir, video_urls, os.path.join(static_path, 'video'))
+            text = save_batch(text, video_dir, (video_urls, video_urls), os.path.join(static_path, 'video'))
             
             target.write(text)
             print(f'{shtml}处理完毕，转换后的新文件为{thtml}')
 
 
+def unzip(path):
+    abs_zip_path = os.path.abspath(path)
+    _source_path = os.path.join(os.path.dirname(abs_zip_path), "tape_snapshot")
+    print(f'压缩文件{abs_zip_path}解压至{_source_path}')
+    os.makedirs(_source_path, exist_ok=True)
+    shutil.unpack_archive(abs_zip_path, _source_path)
+    source_path = os.path.join(_source_path, os.listdir(_source_path)[0])
+    return source_path
+
 if __name__ == '__main__':
     try:
+        # 优先处理命令行参数
+        if len(sys.argv) > 1:
+            source_path = sys.argv[1]
+        # 处理压缩文件
+        if source_path.endswith('zip'):
+            source_path = unzip(source_path)
+        elif os.path.isfile('tape.zip'):
+            source_path = unzip('tape.zip')
+        
+        
         source_path = os.path.join(os.path.abspath(os.curdir), source_path)
         if os.path.isdir(source_path):
             target_path = source_path
@@ -82,11 +103,20 @@ if __name__ == '__main__':
                 print(f'用户{userdir}处理完毕')
             
             print(f'\033[0;36;40m保存完毕，祝你天天开心！\033[0m')
+            print(f'请查看{target_path}目录，原{source_path}已可以删除')
         
         else:
-            print(f'未找到tape目录！请按以下步骤执行:\n')
-            print('\n1. 解压tape导出的zip文件，将解压出的手机号码文件夹重命名为tape\n')
-            print('\n2. 将本exe文件与重命名为tape的文件夹放置于同一目录下\n\t例如D:\\tape 和 D:\\tape_snapshot.exe\n\n')
+            print('未找到tape文件！请参考以下使用说明:')
+            print('\n1.将官网导出的zip文件重命名为tape.zip 或者')
+            print('  解压官网导出的zip文件，将解压出的手机号码文件夹重命名为tape\n')
+            print('\n2. 将本exe文件与重命名为tape的文件夹或tape.zip文件放置于同一目录下\n如：')
+            print('''
+        ────上级目录
+          ├─tape                  【若解压则重命名解压出的手机号码文件夹为tape】
+          ├─tape.zip              【不解压则重命名zip文件】
+          └─tape_snapshot.exe      (本程序)
+                  ''')
+            print('\n3. 运行本程序，保持网络通畅，等待转存完成\n\n')
     except Exception as e:
         print('出现错误：', e)
         print('\033[0;31;40m可联系程序作者\033[0m')
